38468ae955bfb74e7568c6e7e2a3c4427d84c411
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-03-25 22:53:29 +0800

    cleaning up some more code and files

--- zMyAssets/script/vendor.js
+++ zMyAssets/script/vendor.js
@@ -1,14 +1,12 @@
-
-
-
-
 //Should Pull the data from the database
-const balance = document.getElementById("balance-id")
-const usersList = document.getElementById("usersList")
-const foods = document.getElementById("foods")
+const balance = document.getElementById("balance-id");
+const usersList = document.getElementById("usersList");
+const foods = document.getElementById("foods");
 
 // Responsible for the Date time current user on top of the app
-const curDate = document.getElementById("date")
-const curTime = document.getElementById("time")
-const ampm = document.getElementById("ampm")
-const currentUsers = document.getElementById("currentUser")
+const curDate = document.getElementById("date");
+const curTime = document.getElementById("time");
+const ampm = document.getElementById("ampm");
+const currentUsers = document.getElementById("currentUser");
+
+
